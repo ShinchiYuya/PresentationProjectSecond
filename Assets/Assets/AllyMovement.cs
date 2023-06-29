f66886e83b5a202a3c82ebc7199738e2a23a9711
yuya's <134368817+ShinchiYuya@users.noreply.github.com>
2023-06-29 16:15:33 +0900

    味方を右に移動させる 本拠地（スポーンエリア）に当たり判定を付けた

--- Assets/Assets/AllyMovement.cs
+++ Assets/Assets/AllyMovement.cs
@@ -1,76 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class AllyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float attackRange = 5f;
-    [SerializeField] GameObject bulletPrefab = null;
-    [SerializeField] Transform muzzle = null;
-    [SerializeField] Animator animator = null;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float attackRange = 1.5f;
+    [SerializeField] float attackSpeed = 1.5f;
+    [SerializeField] Animator _animator = null;
+    [SerializeField] Transform _muzzle = null;
 
-    Rigidbody2D _rb2d;
-    bool isAttacking = false;
+    private Rigidbody2D rb2d;
+    private bool _isWalking = true;
+    private bool _isAttacking = false;
 
     void Start()
     {
-        _rb2d = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+       _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-
-        Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized;
-        _rb2d.velocity = movement * moveSpeed;
-
-        if (movement.magnitude > 0)
+        if (_isWalking)
         {
-            animator.SetBool("IsMoving", true);
-            animator.SetFloat("MoveX", movement.x);
-            animator.SetFloat("MoveY", movement.y);
+            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
         }
-        else
-        {
-            animator.SetBool("IsMoving", false);
-        }
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-            isAttacking = true;
-            StartCoroutine(Attack());
-        }
-    }
-
-    IEnumerator Attack()
-    {
-        while (isAttacking)
-        {
-            // ìGÇÃåüèo
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, attackRange);
-            foreach (Collider2D collider in colliders)
-            {
-                if (collider.CompareTag("Enemy"))
-                {
-                    // ìGÇ™îÕàÕì‡Ç…Ç¢ÇÈèÍçáÇÕçUåÇÇ∑ÇÈ
-                    GameObject bullet = Instantiate(bulletPrefab, muzzle.position, Quaternion.identity);
-                    Destroy(bullet, 3f);
-                }
-            }
-
-            yield return new WaitForSeconds(1f); // 1ïbÇ≤Ç∆Ç…çUåÇÇåJÇËï‘Ç∑
-        }
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
