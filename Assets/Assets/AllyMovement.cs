4f2250aed2503f8639f20632ff6c897499165587
yuya's <134368817+ShinchiYuya@users.noreply.github.com>
2023-07-01 18:49:41 +0900

    AllyMovementスクリプトの変更

--- Assets/Assets/AllyMovement.cs
+++ Assets/Assets/AllyMovement.cs
@@ -4,27 +4,49 @@ using UnityEngine;
 
 public class AllyMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
-    [SerializeField] float attackRange = 1.5f;
-    [SerializeField] float attackSpeed = 1.5f;
-    [SerializeField] Animator _animator = null;
-    [SerializeField] Transform _muzzle = null;
+    [SerializeField] float _speed = 5f;
+    [SerializeField] float _distanceToCastle = 3f;
 
-    private Rigidbody2D rb2d;
+    private Rigidbody2D _rb2d;
     private bool _isWalking = true;
-    private bool _isAttacking = false;
+
+    private GameObject _enemyCastle;
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-       _animator = GetComponent<Animator>();
+        _rb2d = GetComponent<Rigidbody2D>();
+
+        GameObject[] enemyCastles = GameObject.FindGameObjectsWithTag("EnemyCastle");
+        if (enemyCastles.Length > 0)
+        {
+            float closestDistance = Mathf.Infinity;
+            foreach (GameObject castle in enemyCastles)
+            {
+                float distance = Vector2.Distance(transform.position, castle.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    _enemyCastle = castle;
+                }
+            }
+        }
     }
 
     void Update()
     {
-        if (_isWalking)
+        if (_isWalking && _enemyCastle != null)
         {
-            rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+            float distanceToCastle = Vector2.Distance(transform.position, _enemyCastle.transform.position);
+
+            if (distanceToCastle <= _distanceToCastle)
+            {
+                _rb2d.velocity = Vector2.zero;
+                _isWalking = false;
+            }
+            else
+            {
+                _rb2d.velocity = new Vector2(_speed, _rb2d.velocity.y);
+            }
         }
     }
 }
