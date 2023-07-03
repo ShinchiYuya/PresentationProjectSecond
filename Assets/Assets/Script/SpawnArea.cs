591c065198056535af56196be89a2a3ae25d8224
yuya's <134368817+ShinchiYuya@users.noreply.github.com>
2023-07-03 20:29:30 +0900

    アセットの整理、スポーンスクリプトの作成

---
+++ Assets/Assets/Script/SpawnArea.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArea : MonoBehaviour
+{
+    public GameObject enemyPrefab; // 敵のプレハブ
+    public float spawnInterval = 2f; // 敵の生成間隔
+
+    private Collider2D spawnArea; // 敵生成エリアのコライダー
+    private float spawnTimer = 0f; // スポーンのタイマー
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnArea = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // キーボードの1が押された場合
+        {
+            // エリア内のランダムな位置に敵を生成
+            Vector2 spawnPosition = GetRandomPositionInArea();
+            Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    Vector2 GetRandomPositionInArea()
+    {
+        // エリアの範囲内でランダムな位置を生成
+        Vector2 min = spawnArea.bounds.min;
+        Vector2 max = spawnArea.bounds.max;
+        float randomX = Random.Range(min.x, max.x);
+        float randomY = Random.Range(min.y, max.y);
+        return new Vector2(randomX, randomY);
+    }
+}
