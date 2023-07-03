7a6906851f08130649b64cc52706f497057c97a3
yuya's <134368817+ShinchiYuya@users.noreply.github.com>
2023-07-03 14:49:21 +0900

    武器とそのアニメーションの追加とAllyMovementScriptの基本の追加

--- Assets/Assets/AllyMovement.cs
+++ Assets/Assets/AllyMovement.cs
@@ -1,22 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AllyMovement : MonoBehaviour
 {
     [SerializeField] float _speed = 5f;
-    [SerializeField] float _distanceToCastle = 3f;
-    [SerializeField] Animator _animator = null;
-
+    [SerializeField] float _stopDistance = 1.5f;
+    [SerializeField] Animator _animator;
     private Rigidbody2D _rb2d;
     private bool _isWalking = true;
     private GameObject _enemyCastle;
-
+    private GameObject _enemyCharacter;
     void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _animator.SetBool("Walking", true);
 
+        FindEnemyCastle();
+        FindEnemyCharacter();
+    }
+    void Update()
+    {
+        if (_isWalking)
+        {
+            if (_enemyCastle != null)
+            {
+                float distanceToCastle = Vector2.Distance(transform.position, _enemyCastle.transform.position);
+                if (distanceToCastle > _stopDistance)
+                {
+                    _rb2d.velocity = new Vector2(_speed, _rb2d.velocity.y);
+                    _animator.SetBool("IsWalking", true);
+                }
+                else
+                {
+                    _rb2d.velocity = Vector2.zero;
+                    _animator.SetBool("IsWalking", false);
+                    Attack();
+                }
+            }
+            if (_enemyCharacter != null)
+            {
+                float distanceToCharacter = Vector2.Distance(transform.position, _enemyCharacter.transform.position);
+                if (distanceToCharacter > _stopDistance)
+                {
+                    _rb2d.velocity = new Vector2(_speed, _rb2d.velocity.y);
+                    _animator.SetBool("IsWalking", true);
+                }
+                else
+                {
+                    _rb2d.velocity = Vector2.zero;
+                    _animator.SetBool("IsWalking", false);
+                    Attack();
+                }
+            }
+        }
+    }
+    void FindEnemyCastle()
+    {
         GameObject[] enemyCastles = GameObject.FindGameObjectsWithTag("EnemyCastle");
         if (enemyCastles.Length > 0)
         {
@@ -31,25 +70,35 @@ public class AllyMovement : MonoBehaviour
                 }
             }
         }
-    }
-    void Update()
-    {
-        if (_isWalking && _enemyCastle != null)
+        else
         {
-            float distanceToCastle = Vector2.Distance(transform.position, _enemyCastle.transform.position);
-
-            if (distanceToCastle <= _distanceToCastle)
+            // タグ "EnemyCastle" を持つオブジェクトが見つからなかった場合の処理を記述する（例えばエラーメッセージの表示など）
+        }
+    }
+    void FindEnemyCharacter()
+    {
+        GameObject[] enemyCharacters = GameObject.FindGameObjectsWithTag("Enemy");
+        if (enemyCharacters.Length > 0)
+        {
+            float closestDistance = Mathf.Infinity;
+            foreach (GameObject character in enemyCharacters)
             {
-                _rb2d.velocity = Vector2.zero;
-                _isWalking = false;
-            }
-            else
-            {
-                _rb2d.velocity = new Vector2(_speed, _rb2d.velocity.y);
+                float distance = Vector2.Distance(transform.position, character.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    _enemyCharacter = character;
+                }
             }
         }
-
-        _animator.SetBool("IsWalking", _isWalking);
+        else
+        {
+            // タグ "Enemy" を持つオブジェクトが見つからなかった場合の処理を記述する（例えばエラーメッセージの表示など）
+        }
+    }
+    void Attack()
+    {
+        // 攻撃の処理を記述する
 
     }
 }
